bd098e3ce94473157a05f4710410e8c80041adb0
anabergerr <ana.berger@betrybe.com>
2022-05-21 22:48:31 -0300

    source files

---
+++ aula1.py
@@ -0,0 +1,27 @@
+# math operations
+
+add = 3 + 5
+
+# print(add)
+
+sub = 10 - 2
+
+# print(sub)
+
+div = 16 / 2
+
+# print(div)
+
+mult = 4 * 2
+
+# print(mult)
+
+# Number birthday
+
+fav_number = 13
+
+message = "Happy " + str(fav_number) + "rd Birthday"
+
+print(message)
+
+# create list
